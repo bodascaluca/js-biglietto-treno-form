9440b7347de92b3db1b41530d681da282749f04b
bodascaluca <bodasca.luca@gmail.com>
2022-04-08 16:57:19 +0200

    I have a probleme with console row 6

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const formBtn = document.getElementById("form-btn");
+console.log(formBtn);
+formBtn.addEventListener("click",
+function(){
+    // Prelevo gli imput dell'utente
+    const userName = document.getElementById("user-name").value;
+    console.log(userName);
+
+    const formKm = document.getElementById("formKm").value;
+    console.log(formKm);
+
+    
+
+}
+)
