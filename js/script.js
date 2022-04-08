676f233ceed8ce6c0c8039a0dfe6c22d26196cfb
bodascaluca <bodasca.luca@gmail.com>
2022-04-08 18:20:06 +0200

    ad if for the price

--- js/script.js
+++ js/script.js
@@ -2,14 +2,32 @@ const formBtn = document.getElementById("form-btn");
 console.log(formBtn);
 formBtn.addEventListener("click",
 function(){
-    // Prelevo gli imput dell'utente
-    const userName = document.getElementById("user-name").value;
+    // Prelevo il nome dell utente
+    const userName = document.getElementById("username").value;
     console.log(userName);
 
-    const formKm = document.getElementById("formKm").value;
+    // prelevo i km che deve percorrere
+    const formKm = document.getElementById("formkm").value;
     console.log(formKm);
 
-    
+    // prelevo la fascia d'eta
+    const userAge = document.getElementById("userage").value;
+    console.log(userAge);
+
+    let fullPrice = formKm * 0.21 ;
+
+    if(userAge == `minorenne`){
+        fullPrice = fullPrice - (fullPrice * 20 / 100);
+    } else if (userAge == `over 65`){
+        fullPrice = fullPrice - (fullPrice * 40 / 100);
+    }
+    console.log(fullPrice);
+
+    // Visualizzo nella pagina le risposte degli dell'utente
+    // -inserire le rispsote negli elementi
+    const greetingResponse = document.querySelector(".result h2");
+    console.log(greetingResponse);
+    greetingResponse.innerHTML = `Ciao ${fullPrice}`
 
 }
 )
