b4cedf6d7c5cf9ec9ab838e30d730382b59ea247
bodascaluca <bodasca.luca@gmail.com>
2022-04-11 03:05:51 +0200

    z

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ function(){
     const userName = document.getElementById("username").value;
     console.log(userName);
 
+    //VISUALIZZARE IL NOME 
+    const greetinguserName = document.querySelector(".container-uno h5");
+    greetinguserName.innerHTML = ` ${userName}`;
+    
+
     // prelevo i km che deve percorrere
     const formKm = document.getElementById("formkm").value;
     console.log(formKm);
@@ -25,9 +30,31 @@ function(){
 
     // Visualizzo nella pagina le risposte degli dell'utente
     // -inserire le rispsote negli elementi
-    const greetingResponse = document.querySelector(".result h2");
+    const greetingResponse = document.querySelector(".container-cinque h5");
     console.log(greetingResponse);
-    greetingResponse.innerHTML = `Ciao ${fullPrice}`
+    greetingResponse.innerHTML = ` ${fullPrice}&euro; `;
+    
+    
+
+    
+    
+    // CARROZZA 
+    const randomNumber3 =Math.floor(Math.random()*10)+1;
+    console.log(randomNumber3);
+
+    const greetingResponse3 = document.querySelector(".container-tre h5");
+    console.log(greetingResponse3);
+    greetingResponse3.innerHTML = ` ${randomNumber3} `;
+
+
+    // CODICE CP
+    const randomNumber =Math.floor(Math.random()*100000)+1;
+    console.log(randomNumber);
+
+    const greetingResponse2 = document.querySelector(".container-quattro h5");
+    console.log(greetingResponse2);
+    greetingResponse2.innerHTML = ` ${randomNumber} `;
+
 
 }
 )
